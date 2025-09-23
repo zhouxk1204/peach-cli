ec3127f45524f5fd1b72fbebc7a51a0194841854
周小康 <346960620@qq.com>
2025-09-23 15:02:31 +0800

    feat: init peach-cli

--- bin/peach-cli.js
+++ bin/peach-cli.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-const { runCli } = require("../dist/index.js");
+import { runCli } from '../dist/index.js';
 runCli();
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig(
     {
         entry: "src/index.ts", // 入口文件
-        format: ["cjs"], // 输出格式
+        format: ["esm"], // 输出格式
         clean: true, // 清理输出目录
         outDir: "dist", // 输出目录
         sourcemap: true, // 生成 sourcemap
